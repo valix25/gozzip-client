d426fbae44fefbb705c1562b97b2812e5d1c3266
Valentin Vasiliu <vali15tnt51v@gmail.com>
2021-07-28 17:32:16 +0200

    Initial commit and connect to apollo server

---
+++ src/ApolloProvider.js
@@ -0,0 +1,24 @@
+import React from "react";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
+
+function ApolloProv() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
+
+export default ApolloProv;
