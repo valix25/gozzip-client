4e533658128a0354119880da812a2958807e6374
Valentin Vasiliu <vali15tnt51v@gmail.com>
2021-07-30 00:10:08 +0200

    Connect to heroku deployed server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -11,7 +11,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://mysterious-falls-45439.herokuapp.com/",
 });
 
 // add token to our request
